c3ece01ce097de57d4872ab2fda8f07303b0d368
xaq7 <123476340+xaq7@users.noreply.github.com>
2023-02-04 22:29:15 -0500

    Add files via upload

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Author : xaq <xaq@localhost>
+Author : Zach Valton <xaq@localhost>
 Date   : 2023-02-04
-Purpose: Rock the Casbah
+Purpose: MIS project 1 hello
 """
 
 import argparse
@@ -10,42 +10,10 @@ import argparse
 
 # --------------------------------------------------
 def get_args():
-    """Get command-line arguments"""
+    """Get the command-line arguments"""
 
-    parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', default='World', help='Name to greet')
     return parser.parse_args()
 
 
@@ -54,19 +22,10 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print('Hello, ' + args.name + '!')
 
 
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
+
